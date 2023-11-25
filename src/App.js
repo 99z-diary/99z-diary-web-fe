6a1b25a6a168d966ae971aeeba1b8dee548a235d
yh-project <yoha6865@gmail.com>
2023-11-25 14:34:24 +0900

    Feat/#4: 로그인 페이지 라우터 설정
    로그인 페이지 라우터 설정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Login from "pages/Auth/Login";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <div className="w-[100vw] h-[100vh] bg-gray-100">
-        <Login />
+        <Routes>
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
       </div>
     </>
   );
