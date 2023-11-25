2a3774d5cbbed5ac0ed8894d4c9552b14bb20144
yh-project <yoha6865@gmail.com>
2023-11-25 14:22:28 +0900

    Feat/#4: 로그인 페이지 적용
    App.js 로그인 페이지 적용

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -12,19 +12,19 @@ const Login = () => {
     <>
       <div className="flex flex-col items-center w-[300px] h-fit">
         {/* 타이틀 */}
-        <span className="text-4xl font-bold">99z diary</span>
+        <span className="text-4xl font-semibold">99z diary</span>
         {/* 이름 */}
         <input
-          className="w-full h-12 rounded-[10px] px-4 text-base font-semibold placeholder:text-sm"
+          className="w-full h-12 rounded-[10px] px-4 text-base font-semibold border-2 border-black mt-12 placeholder:text-sm"
           placeholder="이름을 입력해주세요"
         />
         {/* 전화번호*/}
         <input
-          className="w-full h-12 rounded-[10px] px-4 text-base font-semibold placeholder:text-sm"
+          className="w-full h-12 rounded-[10px] px-4 text-base font-semibold border-2 border-black mt-4 placeholder:text-sm"
           placeholder="전화번호를 입력해주세요 (010-1234-5678)"
         />
         {/* 로그인 버튼 */}
-        <button className="w-full h-12 bg-gray-600 rounded-[10px] font-semibold text-white">
+        <button className="w-full h-12 bg-gray-600 rounded-[10px] font-semibold text-white mt-4">
           로그인
         </button>
       </div>
