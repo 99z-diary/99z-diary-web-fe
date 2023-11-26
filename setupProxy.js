d5f94ee9965227432ad17859dac64a70fa0a0938
yh-project <yoha6865@gmail.com>
2023-11-26 22:49:58 +0900

    Feat/#8: 환경 변수 설정
    환경 변수 설정
     - 프록시 수정

--- setupProxy.js
+++ setupProxy.js
@@ -3,7 +3,7 @@ import { createProxyMiddleware } from "http-proxy-middleware";
 module.exports = function (app) {
   app.use(
     createProxyMiddleware("/api", {
-      target: "http://localhost:8080",
+      target: `${process.env.REACT_APP_BACK_URL}`,
       changeOrigin: true,
     })
   );
