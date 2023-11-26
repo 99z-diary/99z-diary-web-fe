44c6b849e0a52049b3837894673514a44ca97904
yh-project <yoha6865@gmail.com>
2023-11-26 22:50:47 +0900

    Feat/#8: 유저 정보 atom
    유저 정보 저장

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { RecoilRoot } from "recoil";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
     {/*<React.StrictMode></React.StrictMode>*/}
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RecoilRoot>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </RecoilRoot>
   </>
 );
 
