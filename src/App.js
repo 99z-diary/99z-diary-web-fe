75dc96a40368983cc503654a554f1e66ec692b6a
yh-project <yoha6865@gmail.com>
2023-11-27 00:02:18 +0900

    Feat/#3: 상단바 디자인
    상단바 디자인

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import TopBar from "components/TopBar";
 import Login from "pages/Auth/Login";
 import { Routes, Route } from "react-router-dom";
 
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
       <div className="w-[100vw] h-[100vh] bg-gray-100">
+        <TopBar />
         <Routes>
           <Route path="/login" element={<Login />}></Route>
         </Routes>
