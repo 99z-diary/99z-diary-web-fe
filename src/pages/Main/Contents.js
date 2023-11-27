46d8d0fd4566cc6d3af677edba7f6662f8874b37
yh-project <yoha6865@gmail.com>
2023-11-27 22:27:46 +0900

    Feat/#12: 조회 api 적용
    Contents 컴포넌트에서 조회 api 사용
     - 렌더링 시 api 수행
     - 더미 대신 서버로부터 받아온 데이터 적용

--- src/pages/Main/Contents.js
+++ src/pages/Main/Contents.js
@@ -1,12 +1,28 @@
 import PostCard from "./PostCard";
 import ScheduleCard from "./ScheduleCard";
-import { postAtom } from "recoil/postAtom";
-import { scheduleAtom } from "recoil/scheduleAtom";
-import { useRecoilValue } from "recoil";
+import { getRecentPost } from "api/postApi";
+import { getRecentSchedule } from "api/scheduleApi";
+import { useState, useEffect } from "react";
 
 const Contents = () => {
-  const post = useRecoilValue(postAtom); // 게시글 더미 데이터
-  const schedule = useRecoilValue(scheduleAtom); // 스케줄 더미 데이터
+  const [posts, setPosts] = useState([]); // 최근 게시글 리스트
+  const [schedules, setSchedules] = useState([]); // 가까운 스케줄 리스트
+  useEffect(() => {
+    // 최근 게시글 조회
+    getRecentPost()
+      .then((response) => {
+        const tmp = [...response.data];
+        setPosts(tmp);
+      })
+      .catch((error) => console.log(error));
+    // 가까운 일정 조회
+    getRecentSchedule()
+      .then((response) => {
+        const tmp = [...response.data];
+        setSchedules(tmp);
+      })
+      .catch((error) => console.log(error));
+  }, []);
   return (
     <div className="w-[840px] h-[800px] rounded-2xl flex flex-col sticky top-24 bg-white shadow-figma">
       {/* 타이틀 */}
@@ -14,26 +30,25 @@ const Contents = () => {
         메인 화면
       </div>
       {/* 최근 글 */}
-      <div className="flex flex-col mx-auto mt-10 w-[784px]">
+      <div className="flex flex-col mx-auto mt-5 w-[784px]">
         <span className="font-bold text-[28px] text-main-title mx-auto">
           최근 글
         </span>
-        <div className="flex items-center justify-between w-full mt-4">
-          {post.map((v, i) => {
-            console.log(v);
-            return <PostCard key={i} target={v} />;
-          })}
+        <div className="flex w-full grid-cols-3 mt-4 gap-x-8">
+          {posts.map((v, i) => (
+            <PostCard key={i} target={v} />
+          ))}
         </div>
       </div>
       {/* 가까운 일정 */}
       <div className="flex flex-col mx-auto mt-9 w-[784px]">
         <span className="font-bold text-[28px] text-main-title mx-auto">
-          디데이 (가까운 날의 방탈 or 놀 계획)
+          디데이
         </span>
-        <div className="flex items-center justify-between w-full mt-4">
-          {schedule.map((v, i) => {
-            return <ScheduleCard key={i} target={v} />;
-          })}
+        <div className="grid w-full grid-cols-3 mt-4 gap-x-5">
+          {schedules.map((v, i) => (
+            <ScheduleCard key={i} target={v} />
+          ))}
         </div>
       </div>
     </div>
